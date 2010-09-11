0257717b4b1a13d76cc371159e0314b26bd50441
Alexander Klink <alech@lou.(none)>
2010-09-11 20:20:46 +0100

    corrected assert parameter order

--- test/test_shamir.rb
+++ test/test_shamir.rb
@@ -7,12 +7,12 @@ class TestShamir < Test::Unit::TestCase
 	def test_instantiation
 		assert_raise( ArgumentError ) { SecretSharing::Shamir.new }
 		s1 = SecretSharing::Shamir.new(5)
-		assert_equal(s1.n, 5)
-		assert_equal(s1.k, 5)
+		assert_equal(5, s1.n)
+		assert_equal(5, s1.k)
 		assert(! s1.secret_set?)
 		s2 = SecretSharing::Shamir.new(5, 3)
-		assert_equal(s2.n, 5)
-		assert_equal(s2.k, 3)
+		assert_equal(5, s2.n)
+		assert_equal(3, s2.k)
 		assert(! s2.secret_set?)
 		assert_raise( ArgumentError ) { SecretSharing::Shamir.new(5, 7) }
 	end
@@ -23,17 +23,17 @@ class TestShamir < Test::Unit::TestCase
 		assert(s.secret_set?)
 		assert_not_nil(s.secret)
 		assert_not_nil(s.shares)
-		assert_equal(s.shares.class, Array)
-		assert_equal(s.shares.length, 5)
-		assert_equal(s.shares[0].class, SecretSharing::Shamir::Share)
-		assert_equal(s.secret_bitlength, DEFAULT_SECRET_BITLENGTH)
+		assert_equal(Array, s.shares.class)
+		assert_equal(5, s.shares.length)
+		assert_equal(SecretSharing::Shamir::Share, s.shares[0].class)
+		assert_equal(DEFAULT_SECRET_BITLENGTH, s.secret_bitlength)
 
 		# can only be called once
 		assert_raise( RuntimeError) { s.create_random_secret() }
 
 		s2 = SecretSharing::Shamir.new(7)
 		s2.create_random_secret(256)
-		assert_equal(s2.secret_bitlength, 256)
+		assert_equal(256, s2.secret_bitlength)
 	end
 
 	def test_recover_secret_k_eq_n
@@ -42,7 +42,7 @@ class TestShamir < Test::Unit::TestCase
 		
 		s2 = SecretSharing::Shamir.new(5)
 		s2.shares << s.shares[0]
-		assert_equal(s2.shares.length, 1)
+		assert_equal(1, s2.shares.length)
 		assert(! s2.secret_set?)
 		assert_nil(s2.secret)
 		# adding the same share raises an error
@@ -65,7 +65,7 @@ class TestShamir < Test::Unit::TestCase
 		
 		s2 = SecretSharing::Shamir.new(5, 3)
 		s2.shares << s.shares[0]
-		assert_equal(s2.shares.length, 1)
+		assert_equal(1, s2.shares.length)
 		assert(! s2.secret_set?)
 		assert_nil(s2.secret)
 		# add more shares
