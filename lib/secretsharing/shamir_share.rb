1a6907bbe086df19690fffffaf584836d71d0024
Glenn Rempe <glenn@rempe.us>
2013-11-30 13:14:59 -0800

    Meet terms of Apache 2.0 License
    - Formalize existing Apache 2.0 License Terms according to license
    requirements.
    - Add LICENSE.txt file with Apache 2.0 license text.
    - Updated contributors.

--- lib/secretsharing/shamir_share.rb
+++ lib/secretsharing/shamir_share.rb
@@ -1,5 +1,18 @@
 # -*- encoding: utf-8 -*-
 
+# Copyright 2011-2013 Alexander Klink and Glenn Rempe
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 module SecretSharing
 
   # A SecretSharing::Shamir::Share object represents a share in the
@@ -93,4 +106,3 @@ module SecretSharing
   end
 
 end
-
