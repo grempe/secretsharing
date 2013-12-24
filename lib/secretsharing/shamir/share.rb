48d4fd73a5082b6d54b4f6133e9a88d3a109ae0d
Glenn Rempe <glenn@rempe.us>
2013-12-23 21:09:20 -0800

    Almost 2014.

--- lib/secretsharing/shamir/share.rb
+++ lib/secretsharing/shamir/share.rb
@@ -1,6 +1,6 @@
 # -*- encoding: utf-8 -*-
 
-# Copyright 2011-2013 Alexander Klink and Glenn Rempe
+# Copyright 2011-2014 Alexander Klink and Glenn Rempe
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
